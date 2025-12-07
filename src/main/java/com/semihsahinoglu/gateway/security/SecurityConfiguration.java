f3eec56ab9b868ab5c0051d4a3682b11f9a8f3e8
Semih Şahinoğlu <semihshngl@gmail.com>
2025-12-07 15:48:36 +0300

    filter ayarlandi

--- src/main/java/com/semihsahinoglu/gateway/security/SecurityConfiguration.java
+++ src/main/java/com/semihsahinoglu/gateway/security/SecurityConfiguration.java
@@ -3,20 +3,33 @@ package com.semihsahinoglu.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
+
+    private final JwtFilter filter;
+
+    public SecurityConfiguration(JwtFilter filter) {
+        this.filter = filter;
+    }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
 
+
         httpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange->exchange.pathMatchers("/api/v1/auth/**").permitAll())
-                .authorizeExchange(exchange->exchange.pathMatchers("/actuator/**").permitAll())
-                .authorizeExchange(exchange->exchange.anyExchange().authenticated());
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .authorizeExchange(exchange -> exchange.pathMatchers("/api/v1/auth/**").permitAll())
+                .authorizeExchange(exchange -> exchange.pathMatchers("/actuator/**").permitAll())
+                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .addFilterAt(filter, SecurityWebFiltersOrder.AUTHENTICATION);
 
         return httpSecurity.build();
     }
